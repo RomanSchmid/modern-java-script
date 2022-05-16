93b7c1dba811284389147723a0a6d495a2592580
Roman_Schmid <rrr.schmid@gmail.com>
2022-05-16 21:54:22 +0200

    Generating HTML divs with name and clear button

--- functions.js
+++ functions.js
@@ -1,7 +1,5 @@
-/*
-    Function retrieving data from the Local Storage
-    Create empty array if the Local Storage is empty
-*/
+/* Function retrieving data from the Local Storage
+Create empty array if the Local Storage is empty */
 
 const getSavedNames = () => {
     const myNames = localStorage.getItem("names")
@@ -13,11 +11,26 @@ const getSavedNames = () => {
     }
 }
 
-/*
-    Function retrieving data from the Local Storage
-    Create empty array if the Local Storage is empty
-*/
+/* Function retrieving data from the Local Storage
+Create empty array if the Local Storage is empty */
 
-const saveNames = (oneName) => {
+const saveNames = oneName => {
     localStorage.setItem("names", JSON.stringify(oneName))
+}
+
+/* Generate HTML structure, which will be placed to the page after clicking the button "To list" */
+
+const generateHTMLStructure = oneName => {
+    const newDiv = document.createElement("div")
+    const newSpan = document.createElement("span")
+    const button = document.createElement("button")
+
+    // Setting delete button
+    button.textContent = "Clear the name"
+    newDiv.appendChild(button)
+
+    newSpan.textContent = oneName.firstName
+    newDiv.appendChild(newSpan)
+
+    return newDiv
 }
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+/* Load data from the Local Storage into the variable
+If the Local Storage is empty -> assign the variable with an empty array */
+
 const names = getSavedNames()
 
+// Send a form and save to the Local Storage
 let myForm = document.querySelector("#test-form")
 myForm.addEventListener("submit", event => {
     event.preventDefault()
@@ -13,4 +17,16 @@ myForm.addEventListener("submit", event => {
 
     saveNames(names)
 
+})
+
+// Listing back to the page
+let buttonToList = document.querySelector(".to-list")
+buttonToList.addEventListener("click", event => {
+    let namesFromStorage = JSON.parse(localStorage.getItem("names"))
+    console.log(namesFromStorage)
+
+    namesFromStorage.forEach(oneName => {
+        const oneNameHTML = generateHTMLStructure(oneName)
+        document.querySelector(".list-names").appendChild(oneNameHTML)
+    })
 })
