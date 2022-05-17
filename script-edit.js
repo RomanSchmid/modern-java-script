766a21abf29a3b0d362b9d144952d11c60f1f236
Roman_Schmid <rrr.schmid@gmail.com>
2022-05-18 00:13:48 +0200

    Treat cases with more editing pages opened

--- script-edit.js
+++ script-edit.js
@@ -19,3 +19,20 @@ changingForm.addEventListener("submit", event => {
 
     saveNames(names)
 })
+
+window.addEventListener("storage", event => {
+
+    if (event.key === "names") {
+        names = JSON.parse(event.newValue)
+    }
+
+    let searchedName = names.find(oneObject => {
+        return oneObject.id === nameID
+    })
+    
+    if (searchedName === undefined) {
+        location.assign("/index.html")
+    }
+    
+    document.querySelector("#editedName").value = searchedName.firstName
+})
